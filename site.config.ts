97d236d5bb1ea7e6fc3810b76cc6ece830a93f30
shouw <2205639587@qq.com>
2026-01-31 15:51:04 +0800

    修改配置信息

--- site.config.ts
+++ site.config.ts
@@ -1,13 +1,13 @@
 import { defineSiteConfig } from 'valaxy'
 
 export default defineSiteConfig({
-  url: 'https://valaxy.site/',
+  url: 'https://shouw.blog/',
   lang: 'zh-CN',
-  title: 'Valaxy Theme Yun',
+  title: 'shouwのblog',
   author: {
-    name: '云游君',
+    name: 'shouw',
   },
-  description: 'Valaxy Theme Yun Preview.',
+  description: '欢迎来到我的个人blog',
   social: [
     {
       name: 'RSS',
@@ -16,86 +16,32 @@ export default defineSiteConfig({
       color: 'orange',
     },
     {
-      name: 'QQ 群 1050458482',
-      link: 'https://qm.qq.com/cgi-bin/qm/qr?k=kZJzggTTCf4SpvEQ8lXWoi5ZjhAx0ILZ&jump_from=webapi',
-      icon: 'i-ri-qq-line',
-      color: '#12B7F5',
-    },
-    {
       name: 'GitHub',
-      link: 'https://github.com/YunYouJun',
+      link: 'https://github.com/TYDON1',
       icon: 'i-ri-github-line',
       color: '#6e5494',
     },
     {
-      name: '微博',
-      link: 'https://weibo.com/jizhideyunyoujun',
-      icon: 'i-ri-weibo-line',
-      color: '#E6162D',
-    },
-    {
-      name: '豆瓣',
-      link: 'https://www.douban.com/people/yunyoujun/',
-      icon: 'i-ri-douban-line',
-      color: '#007722',
-    },
-    {
-      name: '网易云音乐',
-      link: 'https://music.163.com/#/user/home?id=247102977',
-      icon: 'i-ri-netease-cloud-music-line',
-      color: '#C20C0C',
-    },
-    {
-      name: '知乎',
-      link: 'https://www.zhihu.com/people/yunyoujun/',
-      icon: 'i-ri-zhihu-line',
-      color: '#0084FF',
-    },
-    {
       name: '哔哩哔哩',
-      link: 'https://space.bilibili.com/1579790',
+      link: 'https://space.bilibili.com/1777771249',
       icon: 'i-ri-bilibili-line',
       color: '#FF8EB3',
     },
     {
-      name: '微信公众号',
-      link: 'https://cdn.yunyoujun.cn/img/about/white-qrcode-and-search.jpg',
-      icon: 'i-ri-wechat-2-line',
-      color: '#1AAD19',
-    },
-    {
-      name: 'Twitter',
-      link: 'https://twitter.com/YunYouJun',
-      icon: 'i-ri-twitter-x-fill',
-      color: 'black',
-    },
-    {
-      name: 'Telegram Channel',
-      link: 'https://t.me/elpsycn',
-      icon: 'i-ri-telegram-line',
-      color: '#0088CC',
-    },
-    {
       name: 'E-Mail',
-      link: 'mailto:me@yunyoujun.cn',
+      link: 'mailto:kijinseija@shouw.blog',
       icon: 'i-ri-mail-line',
       color: '#8E71C1',
-    },
-    {
-      name: 'Travelling',
-      link: 'https://www.travellings.cn/go.html',
-      icon: 'i-ri-train-line',
-      color: 'var(--va-c-text)',
     },
   ],
 
   search: {
-    enable: false,
+    enable: true,
   },
 
   sponsor: {
     enable: true,
-    title: '我很可爱，请给我钱！',
+    title: '如果我的内容对你有帮助的话可以赞助我一杯咖啡（bushi',
     methods: [
       {
         name: '支付宝',
