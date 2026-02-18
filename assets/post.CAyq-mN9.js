d0377e81902548f27b9cc65f6dba6b5cf55f7f58
TYDON1 <TYDON1@users.noreply.github.com>
2026-02-18 07:32:45 +0000

    deploy: 9f904185513b3128c8ec77fb9224fd2300c66ef6

---
+++ assets/post.CAyq-mN9.js
@@ -0,0 +1,1 @@
+import"./chunks/vue-router.FmWApsIx.js";import{p as t}from"./app.Cv9KF2e9.js";import{y as e}from"./framework.DC7oaBGI.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
---
+++ assets/schema-org.40UJYB57.CVJriMnf.js
@@ -0,0 +1,1 @@
+import{P as t}from"./app.Cv9KF2e9.js";const a=t({cast(r){return r==="number"?{ratingValue:r}:r},defaults:{"@type":"Rating",bestRating:5,worstRating:1}});export{a as r};
