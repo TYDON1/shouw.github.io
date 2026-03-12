851fe37ecdc370cf5dba75d71e4387ba0c1b8c43
TYDON1 <TYDON1@users.noreply.github.com>
2026-03-12 05:49:20 +0000

    deploy: a97b86b516f4cfce3503fc083f0f2af58e3bbdf4

---
+++ assets/back.C_KFVWw6.js
@@ -0,0 +1,1 @@
+import{a as o}from"./chunks/vue-router.D7U_e8Aq.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
