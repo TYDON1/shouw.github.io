6b078cee0ed063e553267ed45a56e8165b8338f9
TYDON1 <TYDON1@users.noreply.github.com>
2026-03-12 05:51:40 +0000

    deploy: 030e02d95498e08b8eda45b161bdcfdb02b02a40

--- assets/post.Bo1v60Vd.js
+++
@@ -1,1 +0,0 @@
-import"./chunks/vue-router.D7U_e8Aq.js";import{p as t}from"./app.9eAXiKV4.js";import{y as e}from"./framework.DC7oaBGI.js";function m(o){if(!o)return{color:"",icon:"",styles:void 0};const r=t();o in r.value.types||(o="link");const s=r.value.types[o].color,i=r.value.types[o].icon,n=e(()=>o?{"--card-c-primary":s}:void 0);return{color:s,icon:i,styles:n}}export{m as u};
