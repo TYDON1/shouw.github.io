e0ad453fbc536c7ce621d2f6a16cc3d8b1ffe51d
TYDON1 <TYDON1@users.noreply.github.com>
2026-04-20 07:57:52 +0000

    deploy: e2cc246ce1b26b4cf9ea199f4f08699289ec5c31

--- assets/back.en9RvveV.js
+++
@@ -1,1 +0,0 @@
-import{a as o}from"./chunks/vue-router.DJ-Gz-Ur.js";function u(){const e=o();function r(){document.referrer&&document.referrer!==location.href?e.back():e.push("/")}return{back:r}}export{u};
